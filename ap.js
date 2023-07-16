c943e9fa0ca4827b80764c05f6f38f4757a8c589
Sadulla <sadulla.khaitov@icloud.com>
2023-07-16 22:09:12 +0500

    Bu mani birinchi kontrolnaya tochkam

---
+++ ap.js
@@ -0,0 +1,23 @@
+// console.log(4/0);
+
+// const a = 5;
+// console.log(a);
+
+// console.log('sadulla' * 7);
+
+// const sadulla = "Sadulla Khaitov haqida malumotlarni olish uchun bu yerni bosing";
+// console.log(sadulla)
+
+// const Khaitov = {
+//     name: "sadulla",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(Khaitov);
+
+// alert("Nima gap");
+// const result = confirm("yaxshimisan")
+
+const answer = prompt("yoshingiz nechida sizni?", "18")
+console.log(answer);
